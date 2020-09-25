1a79abb02ebd67f484ba5b5a165ec9fee8840be8
student <student@o-hara-kashiwa.local>
2020-09-25 11:37:56 +0900

    0924最新

--- diary/forms.py
+++ diary/forms.py
@@ -1,4 +1,23 @@
 from django import forms
+from django.core.mail import EmailMessage
 
 class InquiryForm(forms.Form):
-    pass
+    name = forms.CharField(label='お名前', max_length=30)
+    email = forms.EmailField(label='メールアドレス')
+    title = forms.CharField(label='タイトル', max_length=30)
+    message = forms.CharField(label='メッセージ', widget=forms.Textarea)
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['class'] = 'form-control col-9'
+        self.fields['name'].widget.attrs['placeholder'] = 'お名前をここに入力してください'
+        
+    def send_email(self):
+        subject = 'だー'
+        mail_to = ['fas@roh.shut',]
+        mail_from = 'herkit@sifnj.lsje'
+        body = 'テストメール'
+        mail_msg = EmailMessage(subject=subject, from_email=mail_from, to=mail_to, body=body)
+        mail_msg.send()
+        
+    
